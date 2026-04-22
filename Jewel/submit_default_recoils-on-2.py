f57b66929f1bb9cddafc59630af0d18b03a2d2a1
Leonardo Silva <leonardols.lsilva@usp.br>
2026-04-22 18:00:56 -0300

    Refactor comments and improve error messages

--- Jewel/submit_default_recoils-on-2.py
+++ Jewel/submit_default_recoils-on-2.py
@@ -1,6 +1,6 @@
 #----------------------------------------------
 #
-# artigo de referência das simulações: Jet substructure observables for jet quenching in Quark Gluon Plasma: a Machine Learning driven analysis - https://arxiv.org/abs/2304.07196
+# inspiration: Jet substructure observables for jet quenching in Quark Gluon Plasma: a Machine Learning driven analysis - https://arxiv.org/abs/2304.07196
 #
 #----------------------------------------------
 
@@ -12,7 +12,7 @@ import shutil
 import time
 
 #----------------------------------------------#
-#                  FUNÇÕES                     #
+#                  FUNCTIONS                   #
 #----------------------------------------------#
 
 def safe_remove(path, retries=5, delay=1):
@@ -53,67 +53,7 @@ def execute_command(command):
         return e.stderr, e.stdout
 
 #----------------------------------------------
-#----------------------------------------------
 
-"""
-def convert_hepmc_to_pu14(hepmc_file, sim_type):
-    base_dir = os.getcwd()
-
-    root_file = os.path.join(base_dir, os.path.basename(hepmc_file.replace(".hepmc", ".root")))
-    pu14_file = os.path.join(base_dir, os.path.basename(hepmc_file.replace(".hepmc", ".pu14")))
-
-    # Convert HEPMC to ROOT
-    if os.path.exists(hepmc_file):
-        root_command = f"./hepmc_to_root {hepmc_file} {root_file}"
-        execute_command(root_command)
-        if not os.path.exists(root_file):
-            raise FileNotFoundError(f"ROOT file {root_file} was not created.")
-        safe_remove(hepmc_file)  # Delete HEPMC after successful ROOT creation
-        print(f"File {hepmc_file} deleted after conversion to ROOT.")
-    else:
-        raise FileNotFoundError(f"HEPMC file {hepmc_file} does not exist.")
-
-    # Convert ROOT to PU14
-    root_to_pu14_command = f"./root_to_pu14 {root_file} {pu14_file}"
-    execute_command(root_to_pu14_command)
-    if not os.path.exists(pu14_file):
-        raise FileNotFoundError(f"PU14 file {pu14_file} was not created.")
-    safe_remove(root_file)  # Delete ROOT after successful PU14 creation
-    print(f"File {root_file} deleted after conversion to PU14.")
-
-    return pu14_file
-
-#----------------------------------------------
-
-def analyze_pu14(pu14_file, experiment_num, background):
-    # Definir o nome do arquivo de saída como ROOT
-    output_root = pu14_file.replace(".pu14", "_structure.root")
-    analysis_script = "/sampa/llimadas/Jewel2.4/analysis/doStructure.py"
-
-    if (background == "nobkg"):
-        analysis_command = f"python {analysis_script} -i {pu14_file} -o {output_root} -n 2000"
-    elif (background == "bkg"):
-        background_file = f"/sampa/llimadas/Jewel2.4/analysis/thermalEvent/results/ThermalEventsMult7000PtAv1.20_{experiment_num}.pu14"
-        analysis_command = f"python {analysis_script} -i {pu14_file} -b{background_file} -o {output_root} -n 2000"
-        
-    execute_command(analysis_command)
-    if not os.path.exists(output_root):
-        raise FileNotFoundError(f"Output ROOT file {output_root} was not created.")
-    else:
-        os.chmod(output_root, 0o644)
-        shutil.move(output_root, f"/sampa/llimadas/Jewel2.4/analysis/results/{os.path.basename(output_root)}") # -> salva o ROOT em uma pasta de resultados
-
-    # Excluir o arquivo .pu14 após a conversão
-    if os.path.exists(pu14_file):
-        safe_remove(pu14_file)
-        print(f"File {pu14_file} deleted after analysis.")
-
-    print(f"Analysis completed for {pu14_file}. Output ROOT file generated at {output_root}.")
-    return output_root
-"""
-
-#----------------------------------------------
-#----------------------------------------------
 
 def analyze_hepmc(hepmc_file, experiment_num, jet_type, background, MLtype):
     analysis_script = "/sampa/llimadas/Jewel2.4/analysis/doStructure"
@@ -123,27 +63,25 @@ def analyze_hepmc(hepmc_file, experiment_num, jet_type, background, MLtype):
     elif background == 'bkg':
         background_flag = 'bkgsub'
     else:
-        raise ValueError("background inválido. Use 'nobkg' ou 'bkg'.")
+        raise ValueError("Invalid background. Use 'nobkg' or 'bkg'.")
 
 
     base_name = os.path.splitext(os.path.basename(hepmc_file))[0]
     output_root_name = f"{base_name}_{MLtype}_structure"
-    output_root_path = os.path.join(os.getcwd(), output_root_name + f"_full_{background_flag}.root") # doStructure adiciona estes nomes
+    output_root_path = os.path.join(os.getcwd(), output_root_name + f"_full_{background_flag}.root") 
 
     # 
     analysis_command = f"{analysis_script} --{jet_type} --{background_flag} {hepmc_file} {output_root_name}"
     execute_command(analysis_command)
 
-    # confere se o arquivo foi criado
     if not os.path.exists(output_root_path):
         raise FileNotFoundError(f"Arquivo ROOT {output_root_path} não foi criado.")
 
-    # muda pro destino final
     final_path = "/sampa/llimadas/Jewel2.4/analysis/results_root/"
     move_command = f"mv {output_root_path} {final_path} "
     execute_command(move_command)
 
-    print(f"Análise finalizada.")
+    print(f"Done.")
     return
 
 
@@ -196,8 +134,6 @@ def run_simulation(experiment_num, nevent, background, MLtype):
     if not os.path.exists(hepmc_file):
         raise FileNotFoundError(f"HEPMC file {hepmc_file} was not created.")
 
-    #pu14_file = convert_hepmc_to_pu14(hepmc_file, sim_type="medium")
-    #output_root = analyze_pu14(pu14_file, experiment_num, background) #final file -> results are here!
     analyze_hepmc(hepmc_file, experiment_num, 'fulljets', background, MLtype)
     return 
 
@@ -232,8 +168,6 @@ def run_vacuum_simulation(experiment_num, nevent, background, MLtype):
     if not os.path.exists(hepmc_file):
         raise FileNotFoundError(f"HEPMC file {hepmc_file} was not created.")
 
-    #pu14_file = convert_hepmc_to_pu14(hepmc_file, sim_type="medium")
-    #output_root = analyze_pu14(pu14_file, experiment_num, background) #final file -> results are here!
     analyze_hepmc(hepmc_file, experiment_num, 'fulljets', background, MLtype)
     return
 
